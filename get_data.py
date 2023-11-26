980f2d3f0e6acd3369818da9bb3b83077542a7f4
xuanle811 <phamthixuan811@gmail.com>
2023-11-26 16:46:20 +0700

    change getdata.py'

--- get_data.py
+++ get_data.py
@@ -1,12 +1,12 @@
-import os
-import wget
+# import os
+# import wget
 
-# data from https://www.sciencedirect.com/science/article/pii/S2352340920303048
+# # data from https://www.sciencedirect.com/science/article/pii/S2352340920303048
 
-# Download the zipped dataset
-url = 'https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/383116/rawdata_new.csv?sequence=1&isAllowed=y'
-csv_name = "data_raw.csv"
-wget.download(url, csv_name)
+# # Download the zipped dataset
+# url = 'https://data.mendeley.com/public-files/datasets/yshdbyj6zy/files/6722d2a1-b669-427e-9c1e-f1360ad485b0/file_downloaded'
+# csv_name = "data_raw.csv"
+# wget.download(url, csv_name)
 
 # Unzip it and standardize the .csv filename
 # import zipfile
@@ -15,3 +15,15 @@ wget.download(url, csv_name)
 #     zip_ref.extract(zip_ref.filelist[0])
 
 # os.remove(zip_name)
+
+
+
+# Xuan viet
+# Đọc nội dung từ file first-data
+with open('first-data.csv', 'r') as first_file:
+    data = first_file.read()
+
+# Tạo một file mới có tên là second-data và ghi nội dung vào đó
+with open('data_raw.csv', 'w') as second_file:
+    second_file.write(data)
+    
